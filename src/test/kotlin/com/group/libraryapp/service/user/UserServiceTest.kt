b7e5b61e9b6f99267c235c7a81042d7b6452f8bb
yunhwan <147581818+yunhwane@users.noreply.github.com>
2024-09-19 00:03:14 +0900

    코틀린으로 유저 서비스 테스트 코드 작성

--- src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -1,8 +1,13 @@
 package com.group.libraryapp.service.user
 
+import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.tuple
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -13,7 +18,14 @@ class UserServiceTest @Autowired constructor(
     private val userRepository: UserRepository,
     private val userService: UserService
 ) {
+
+    @AfterEach
+    fun clean() {
+        userRepository.deleteAll()
+    }
+
     @Test
+    @DisplayName("유저 저장이 정상적으로 동작한다.")
     fun saveUserTest() {
         //given
         val userCreateRequest = UserCreateRequest("전윤환", null)
@@ -29,4 +41,54 @@ class UserServiceTest @Autowired constructor(
      * 코틀린에서는 null 이 아닐것이라고 생각하고 가져옴
      * 따라서 자바 코드에서 @nullable 어노테이션을 붙여야됨
      */
+
+    @Test
+    @DisplayName("유저 조회가 정상적으로 동작한다.")
+    fun getUsersTest() {
+        //given
+        userRepository.saveAll(
+            listOf(
+                User("A", null),
+                User("B", 27)
+            )
+        )
+        //when
+        val results = userService.getUsers()
+        //then
+        assertThat(results).hasSize(2)
+
+        assertThat(results)
+            .extracting("name", "age")
+            .containsExactly(
+                tuple("A", null),
+                tuple("B", 27)
+            )
+    }
+
+    @Test
+    @DisplayName("유저 이름 수정이 정상적으로 동작한다.")
+    fun updateUserNameTest() {
+        //given
+        val savedUser = userRepository.save(User("A", null))
+        val request = UserUpdateRequest(savedUser.id, "B")
+
+        //when
+        userService.updateUserName(request)
+
+        //then
+        val result = userRepository.findById(savedUser.id).get()
+        assertThat(result.name).isEqualTo("B")
+    }
+
+    @Test
+    @DisplayName("유저 삭제 기능이 정상적으로 동작한다.")
+    fun deleteUserTest() {
+        //given
+        userRepository.save(User("A", null))
+        //when
+        userService.deleteUser("A")
+        //then
+        val results = userRepository.findAll()
+        assertThat(results).isEmpty()
+    }
 }
